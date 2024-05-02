9b8f910705b27c9b4254c725e1589bca540dd67f
Uğur Alp <38879240+UAt23@users.noreply.github.com>
2024-05-02 11:19:11 +0300

    Provider and Routes set

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import dataProvider, {
-  GraphQLClient,
-  liveProvider,
-} from "@refinedev/nestjs-query";
+import { authProvider, dataProvider, liveProvider } from "./providers";
+import { Home, ForgotPassword, Login, Register} from "./pages";
+
+
 import routerBindings, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
@@ -25,9 +25,11 @@ function App() {
           <AntdApp>
             <DevtoolsProvider>
               <Refine
+                dataProvider={dataProvider}
+                liveProvider={liveProvider}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
-                // authProvider={}
+                authProvider={authProvider}
                 options={{
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
@@ -38,6 +40,10 @@ function App() {
               >
                 <Routes>
                   <Route index element={<WelcomePage />} />
+                  <Route index element={<Home />}/>
+                  <Route path="/register" element={<Register />}/>
+                  <Route path="/login" element={<Login />}/>
+                  <Route path="/forgot-password" element={<ForgotPassword />}/>
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
